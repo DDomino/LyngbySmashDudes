13b3308eb59988eb8a07256322eef1c226eadbf1
DDomino <Andreasj54@gmail.com>
2019-06-04 10:21:28 +0200

    added Exit button and fixed cam

---
+++ Assets/shutdown.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shutdown : MonoBehaviour
+{
+
+public void ShutDown()
+    {
+
+        Application.Quit();
+
+    }
+
+
+
+}
